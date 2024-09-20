d9179b00619e39c515a0cebb0564912058df923c
Neil joseph <161617321+Ronin-117@users.noreply.github.com>
2024-09-20 16:20:51 +0530

    base code

---
+++ agents.py
@@ -0,0 +1,45 @@
+from crewai import Agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+## call the gemini models
+llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                           verbose=True,
+                           temperature=0.5,
+                           google_api_key="AIzaSyBZknRAIpjSxW0VvGqQSuBLWOWWONJp1Dg"
+)
+
+# Creating a senior researcher agent with memory and verbose mode
+
+news_researcher=Agent(
+    role="Senior Researcher",
+    goal='Unccover ground breaking technologies in {topic}',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Driven by curiosity, you're at the forefront of"
+        "innovation, eager to explore and share knowledge that could change"
+        "the world."
+
+    ),
+    llm=llm,
+    allow_delegation=True
+
+)
+
+## creating a write agent with custom tools responsible in writing news blog
+
+news_writer = Agent(
+  role='Writer',
+  goal='Narrate compelling tech stories about {topic}',
+  verbose=True,
+  memory=True,
+  backstory=(
+    "With a flair for simplifying complex topics, you craft"
+    "engaging narratives that captivate and educate, bringing new"
+    "discoveries to light in an accessible manner."
+  ),
+  llm=llm,
+  allow_delegation=False
+)
